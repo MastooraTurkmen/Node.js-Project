1630653b98408a951fbba584f3a2593bbcf9694f
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-05 19:03:34 +0430

    set up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         
       </header>
     </div>
