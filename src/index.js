d949bbf988cbb9c2219f200c565cf9f3672c4b31
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-06 19:16:03 +0430

    set up

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
