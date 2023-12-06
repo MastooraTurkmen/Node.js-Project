70df6fb361a8fcaa3d17a934c2424a916516b21b
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-06 19:16:22 +0430

    set up

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
