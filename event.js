61c43f7c24547875632749dd7fb23b3b579793fc
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-20 10:39:10 +0430

    Node Event Emitter

--- event.js
+++ event.js
@@ -2,19 +2,22 @@ const EventEmitter = require('events');
 const celebrity = new EventEmitter();
 
 // Subscribe to celebrity for Observer 1
-celebrity.on('race win', function () {
-    console.log('Congratulations! You are the best!');
+celebrity.on('race', (results) => {
+    if (results === 'win') {
+        console.log('Congratulations! You are the best!');
+    }
 })
 
 // Subscribe to celebrity for Observer 2
-celebrity.on('race win', function () {
-    console.log('Boo! I could have done better than that!');
+celebrity.on('race', (results) => {
+    if (results === 'win') {
+        console.log('Boo! I could have done better than that!');
+    }
 })
 
 process.on('exit', (code) => {
     console.log('Process exit event with code:', code);
 });
 
-celebrity.emit('race win');
-celebrity.emit('race lost');
-celebrity.emit('race win');
+celebrity.emit('race', 'win');
+celebrity.emit('race', "lost");
