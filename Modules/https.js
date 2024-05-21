f7694719e928e68752d852b6ce463d7ade6c676f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-21 17:07:55 +0430

    Modules

--- Modules/https.js
+++ Modules/https.js
@@ -1,3 +1,10 @@
-function request(url, data) {
-    
+const { send } = require('./request.js');
+const { read } = require('./response.js')
+
+function make(url, data) {
+    send(url, data);
+    return read();
 }
+
+const responseData = make('https://google.com', 'hello');
+console.log(responseData);
