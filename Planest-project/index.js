ac756a67751944201873df7eea143c455c64d751
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-22 21:23:13 +0430

    planest project

---
+++ Planest-project/index.js
@@ -0,0 +1,32 @@
+const { parse } = require('csv-parse');
+const fs = require('fs')
+
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data)
+        }
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        const mapOver = habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        })
+        console.log(mapOver);
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+    });
+// parse()
