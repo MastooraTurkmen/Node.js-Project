df1e74a12b1769973c6bec5e171217befb87d518
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-23 15:55:59 +0430

    http server

---
+++ HTTP-Server/index.js
@@ -0,0 +1,54 @@
+const http = require('http');
+
+const PORT = 3000;
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: "Nikola Tesla"
+    },
+    {
+        id: 1,
+        name: "Sir Isaac Newton"
+    },
+    {
+        id: 2,
+        name: "Albert Einstein"
+    }
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/')
+    // /friends/2 => ['','friends', '2']
+    if (items[1] === 'friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+
+    } else if (items[1] === 'messages') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<url>')
+        res.write('<li>Hello Isaac!</li>')
+        res.write('<li>What are your thoughts on astronomy!</li>')
+        res.write('</url>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end()
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
