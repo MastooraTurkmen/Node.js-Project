88abbd2e9ae5aad2af432198dbf47d1a027038e4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-25 16:07:18 +0430

    express js

--- Express.js-Project/server.js
+++ Express.js-Project/server.js
@@ -9,20 +9,43 @@ const friends = [
     },
     {
         id: 1,
-        name: 'Sir Isaac Newton'
+        name: 'Sir Isaac Newtoon'
     }
 ];
 
-app.get('/friends', (req, res) => {
-    res.json(friends);
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        })
+    }
+    const newFriends = {
+        name: req.body.name,
+        id: friends.length
+    }
+    friends.push(newFriends);
+    res.json(newFriends)
 })
 
+app.get('/friends', (req, res) => {
+    res.json(friends);
+});
+
 // Get /friends/22
-app.get('/friends/:friendId', () => {
+app.get('/friends/:friendId', (req, res) => {
     const friendId = Number(req.params.friendId);
     const friend = friends[friendId]
     if (friend) {
-        res.json(200).json(friend);
+        res.status(200).json(friend);
     } else {
         res.status(404).json({
             error: "Friend does not exist"
