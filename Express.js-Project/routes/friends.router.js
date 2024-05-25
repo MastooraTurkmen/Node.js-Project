ed23489275d2afbabceb9536e065572caf238b07
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-25 19:01:26 +0430

    friends router

--- Express.js-Project/routes/friends.router.js
+++ Express.js-Project/routes/friends.router.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const friendController = require('./controllers/friends');
+const friendController = require('../controllers/friends.controller');
 const friendsRouter = express.Router();
+
+friendsRouter.use((req, res, next) => {
+    console.log("ip address", req.ip);
+    next()
+});
 
 friendsRouter.post('/', friendController.postFriend);
 friendsRouter.get('/', friendController.getFriends);
